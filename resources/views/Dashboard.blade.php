968e09aef0d2460c9465f7d0bb1699e7ecd2f9f2
Dinesh <s3636468@student.rmit.edu.au>
2018-09-16 16:10:44 +1000

    Removed borders

--- resources/views/Dashboard.blade.php
+++ resources/views/Dashboard.blade.php
@@ -234,38 +234,38 @@ ul.topnav li.right {float: right;}
         			
         			<tr>
         		<td><label>Product Type: </label></td>
-        		<td><input type="text" id="type" style="background-color: rgb(33,35,39); color: white; border-color: rgb(33,35,39);" readonly ></td>
+        		<td><input type="text" id="type" style="background-color: rgb(33,35,39); color: white; border-color: rgb(33,35,39); border: 0px;" readonly ></td>
         			</tr>
         			
         			<tr>
         		
         		<td><label>Total Quantity: </label></td>
-        		<td><input type="text" id="tqty" style="background-color: rgb(33,35,39); color: white; border-color: rgb(33,35,39);"readonly></td>
+        		<td><input type="text" id="tqty" style="background-color: rgb(33,35,39); color: white; border-color: rgb(33,35,39); border: 0px;"readonly></td>
         			</tr>
         			
         			<tr>
         		<td><label>Unit Price: </label></td>
-        		<td><input type="text" id="unitPrice" style="background-color: rgb(33,35,39); color: white; border-color: rgb(33,35,39);" readonly></td>
+        		<td><input type="text" id="unitPrice" style="background-color: rgb(33,35,39); color: white; border-color: rgb(33,35,39); border: 0px;" readonly></td>
         			</tr>
 
         			<tr>
         		<td><label>Total Price: </label></td>
-        		<td><input type="text" id="totalPrice" style="background-color: rgb(33,35,39); color: white; border-color: rgb(33,35,39);"readonly></td>
+        		<td><input type="text" id="totalPrice" style="background-color: rgb(33,35,39); color: white; border-color: rgb(33,35,39); border: 0px;"readonly></td>
         			</tr>
 
         			<tr>
         		<td><label>Expiry: </label></td>
-        		<td><input type="text" id="expiry" style="background-color: rgb(33,35,39); color: white; border-color: rgb(33,35,39);" readonly></td>
+        		<td><input type="text" id="expiry" style="background-color: rgb(33,35,39); color: white; border-color: rgb(33,35,39); border: 0px;" readonly></td>
         			</tr>
 
         			<tr>
         		<td><label>Vintage: </label></td>
-        		<td><input type="text" id="vintage" style="background-color: rgb(33,35,39); color: white; border-color: rgb(33,35,39);" readonly></td>
+        		<td><input type="text" id="vintage" style="background-color: rgb(33,35,39); color: white; border-color: rgb(33,35,39); border: 0px;" readonly></td>
         			</tr>
 
         			<tr>
         		<td><label>Condition: </label></td>
-        		<td><input type="text" id="condition" style="background-color: rgb(33,35,39); color: white; border-color: rgb(33,35,39);" readonly></td>
+        		<td><input type="text" id="condition" style="background-color: rgb(33,35,39); color: white; border-color: rgb(33,35,39); border: 0px;" readonly></td>
         			</tr>
         	</table>
       </div>
