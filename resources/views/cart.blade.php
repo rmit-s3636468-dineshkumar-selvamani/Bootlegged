a0b9bb5a966bba57590bca0267d3fa93ed21d316
rmit-s3649980-Naveen-SivaKumar <s3649980@student.rmit.edu.au>
2018-10-21 14:13:20 +1100

    Update cart.blade.php

--- resources/views/cart.blade.php
+++ resources/views/cart.blade.php
@@ -324,7 +324,28 @@
     <body>
 
 
-    @include('sideNavBar')
+      <div class="sidebar">
+  <img style=" position: absolute; display: inline;" class="logo"  alt ="logo" src="/Images/logo1.png">
+  <a  href="#home" style="margin-top: 90px; color: white; text-align: center;">Welcome,<br> {{Auth::user()->business_name}}</a>
+   
+  @if( Auth::user()->type == 'StoreOwner')
+  <a  href="/home" style="color: white;">Market Place</a>
+   @endif
+  <a href="/mylistings" style="color: white;">My Listing</a>
+  <a href="/uploadchoose" style="color: white;">Add Listing</a>
+  <a href="/history" style="color: white;">History</a>
+        @if( Auth::user()->type == 'StoreOwner')
+            <a href="slowstock" style="color: white;">Slow Movers</a>
+            <a href="opportunities" style="color: white;">Opportunities</a>
+        @endif
+   <hr style="border-style: groove;
+    border-width: 1px;"> 
+  <a href="/editProfile" style="color: white;">Edit Profile</a>
+  @if( Auth::user()->type == 'StoreOwner')
+  <a href="/cart" style="color: white;" class="active">My Cart <span class="badge badge-warning">{{ Session::has('cart') ? Session::get('cart')->totalQuantity : '' }}</span></a>
+  @endif
+  <a href="{{URL::to('logout')}}" style="color: white;">Logout</a>
+</div>
 
 
 
