60654081c1d538a40c3dc7b54dfa0a5a2f239b1c
rmit-s3649980-Naveen-SivaKumar <s3649980@student.rmit.edu.au>
2018-10-21 13:29:38 +1100

    Update faq.blade.php

--- resources/views/faq.blade.php
+++ resources/views/faq.blade.php
@@ -1,17 +1,18 @@
  <div class="faq" id="faq">
         
         <p style=" font-size: 40px;"> FAQ's </p><br>
-          <img src="Images/faq_image.png" style="float: right; height: 700px; width:500px; margin-right: 50px; margin-top: -80px;">
-          <p class="questions"> What is Bootlegged?</p>
-          <p class="answers"> Lorem ipsum dolor sit amet, sea te dolor commune, odio unum ne vix, sea vidit scriptorem ne.Lorem ipsum dolor sit amet, sea te dolor commune, odio unum ne vix, sea vidit scriptorem ne. Ius et suas delectus iudicabit.</p>
-          <p class="questions"> How to contact us?</p>
-          <p class="answers">Lorem ipsum dolor sit amet, sea te dolor commune, odio unum ne vix, sea vidit scriptorem ne. Ius et suas delectus iudicabit.</p>
-          <p class="questions"> What are the price options?</p>
-          <p class="answers"> Lorem ipsum dolor sit amet, sea te dolor commune, odio unum ne vix.sea vidit scriptorem ne.Lorem ipsum dolor sit amet, sea te dolor commune, odio unum ne vix, sea vidit scriptorem ne. Ius et suas delectus iudicabit.</p>
-          <p class="questions"> Lorem ipsum dolor sit amet, sea te dolor commune</p>
-          <p class="answers"> Lorem ipsum dolor sit amet, sea te dolor commune, odio unum ne vix.sea vidit scriptorem ne.Lorem ipsum dolor sit amet, sea te dolor commune, odio unum ne vix, sea vidit scriptorem ne. Ius et suas delectus iudicabit.</p>
-          <p class="questions"> Lorem ipsum dolor sit amet, sea te dolor commune?</p>
-          <p class="answers">Lorem ipsum dolor sit amet, sea te dolor commune, odio unum ne vix.sea vidit scriptorem ne.Lorem ipsum dolor sit amet, sea te dolor commune, odio unum ne vix, sea vidit scriptorem ne. Ius et suas delectus iudicabit.</p>
+          <img src="Images/faq_image.png" style="float: right; height: 900px; width:650px; margin-right: 30px; margin-top: -80px;">
+          <p class="questions"> Why should I purchase stock on BOOTLEGGED?</p>
+          <p class="answers"> BOOTLEGGED offers pricing below cost in order to move stock quickly. As stock approaches expiry prices will drop. BOOTLEGGED has inventory information relating to more outlets in Australia than any other marketplace.
+BOOTLEGGED connects shop owners and suppliers across a network. BOOTLEGGED may be used as an alternate supply chain for supplier out stocks, manufacturer cannot supply lines and discontinued lines.</p>
+          <p class="questions">What stock can I list on BOOTLEGGED?</p>
+          <p class="answers">Any retail products and on-premise bulk sold in retail liquor and on-premise locations. Cold chain can be listed but at this stage no delivery option is available through the marketplace. The seller must arrange cold chain supply and ensure supply does not breach cold chain requirements for that product.</p>
+          <p class="questions"> What fees are charged by BOOTLEGGED?</p>
+          <p class="answers">A low $10 monthly fee for free stock transfers, free to list unlimited items for sale and free to buy. A 5% commission fee (ex GST) plus the credit card fee is applied to each transaction (ex GST). If an item list price exceeds $100 then the commission is CAPPED AT $5 per item over $100. There is no minimum commission.</p>
+          <p class="questions">How do I pay for stock bought on BOOTLEGGED?</p>
+          <p class="answers">BOOTLEGGED uses a trusted third-party, Stripe, to provide integrated online merchant services for all standard and premium domestic credit cards.</p>
+          <p class="questions">How secure is the information I provide upon registration?</p>
+          <p class="answers">The BOOTLEGGED platform is hosted in a Sydney-based data centre using Amazon Web Services. Our system applies industry standard security practices to ensure that data is securely managed. We use SSL to ensure that data transferred between our data centre and you is encrypted.</p>
 
 
       </div>
