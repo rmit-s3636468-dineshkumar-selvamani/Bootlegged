cb62865fe2585210d6154a91dd1012300b21130d
Theo <theodore.lch@gmail.com>
2018-11-25 12:26:15 +1100

    Update alpha: refactor web.php routing

--- routes/web.php
+++ routes/web.php
@@ -15,123 +15,125 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-
-
-// Route::get('home', function(){
-//     return View('welcome'); // Your Blade template name
-// });
-Route::get('registerman', function(){
-    return View('auth/registerman'); // Your Blade template name
+Route::get('welcome', function () {
+    return redirect('welcome');
 });
 
-
-Route::get('registersto', function(){
-    return View('auth/registersto'); // Your Blade template name
+Route::get('login', function () {
+    return View('auth/login');
 });
 
-Route::get('choose', function(){
-    return View('choose'); // Your Blade template name
-});
-	
-Route::get('login', function(){
-    return View('auth/login'); // Your Blade template name
+Route::post('login', function () {
+    return View('auth/login');
 });
 
+Route::get('registerman', function () {
+    return View('auth/registerman');
+});
+
+Route::get('registersto', function () {
+    return View('auth/registersto');
+});
 
 Route::post('/register_man', ['as' => 'register_man', 'uses' => 'Auth\ManufacturerController@register']);
 
 Route::post('/register_sto', ['as' => 'register_sto', 'uses' => 'Auth\StoreController@register']);
 
-
-// Route::post('/login_man','loginmanuController@login');
-
-Route::get('welcome', function () {
-    return redirect('welcome');
-});
-
 Auth::routes();
 
-Route::get('/home', 'HomeController@index')->name('home');
+// Routes for Authenticated Users Only
+Route::group(['middleware' => 'auth'], function () {
 
-Route::get('logout', '\App\Http\Controllers\Auth\LoginController@logout');
+    /* ----- Home Controller ----- */
+    Route::get('/home', 'HomeController@index')->name('home');
 
-Route::get('/mylistings', 'MyListingsController@index')->name('mylistings');
+    Route::get('/filter/{id}', 'HomeController@filter')->name("dashboard/filter");
 
-Route::get('/addlistings', 'AddListingsController@index')->name('addlistings');
+    //For enter function in Auto suggest
+    Route::get('/filter', 'HomeController@filterEnter')->name("dashboard/filter");
+
+    Route::get('/filterName/{id}', 'HomeController@filterName')->name("dashboard/filter");
+
+    Route::get('/user/find', 'HomeController@searchUsers');
+
+    /* ----- EditProfile Controller ----- */
+    Route::get('/editProfile', 'EditProfileController@index')->name('editProfile');
+
+    Route::post('/store_editProfile', ['as' => 'store_editProfile', 'uses' => 'EditProfileController@saveStore']);
+
+    Route::post('/manu_editrofile', ['as' => 'manu_editProfile', 'uses' => 'EditProfileController@saveManu']);
+
+    /* ----- History Controller ----- */
+    Route::get('/history', 'HistoryController@index');
+
+    /* ----- Opportunity Controller ----- */
+    Route::get('/opportunities', 'OpportunityController@opportunityWithRanking')->name('opportunities');
+
+    /* ----- SlowStock Controller ----- */
+    Route::get('/slowstock', 'SlowStockController@index');
+
+    /* ----- MyListings Controller ----- */
+    Route::get('/mylistings', 'MyListingsController@index')->name('mylistings');
+
+    // Delete Product
+    Route::post('removeproduct', 'MyListingsController@remove')->name('mylistings');
+
+    Route::post('update', 'MyListingsController@saveprod')->name('mylistings');
+
+    /* ----- AddListings Controller ----- */
+    Route::get('/addlistings', 'AddListingsController@index')->name('addlistings');
+
+    Route::get('/addlistingFromSlow', 'AddListingsController@addFromSlow');
+
+    //Add new product
+    Route::post('create', 'AddListingsController@insert');
+
+    //Autofill Product Type
+    Route::get('/autofill/{id}', 'AddListingsController@autofillType')->name("Addlisting/");
+
+    /* ----- AddNewProduct Controller ----- */
+    Route::post('/createnewprod', ['as' => 'createnewprod', 'uses' => 'AddNewProductController@savenewprod']);
+
+    Route::get('newprod', function () {
+        return View('newprod');
+    });
+
+    /* ----- FormUpload Controller ----- */
+    //File Upload
+    Route::get('uploadchoose', function () {
+        return View('uploadchoose');
+    });
+
+    Route::get('/upload', 'FormuploadController@index');
+
+    Route::post('upload', 'FormuploadController@upload');
+
+    /* ----- DownloadFile Controller ----- */
+    Route::get('/downloads', 'DownloadFileController@download');
+
+    /* ----- Cart Controller ----- */
+    //Cart Index
+    Route::get('/cart', 'CartController@index')->name('cart.index');
+
+    // Add item to cart via get
+    Route::get('/addToCart/{id}', 'CartController@addToCart')->name('cart.add-item');
+
+    // Minus quantity function
+    Route::post('/updateItem', 'CartController@updateItem')->name('cart.update');
+
+    // Remove Item
+    Route::get('/removeItem/{id}', 'CartController@removeItem')->name('cart.remove');
+
+    // Clear Cart
+    Route::get('/clearCart', 'CartController@clearCart')->name('cart.clear');
+
+    /* ----- StripeCheckout Controller ----- */
 
 
-Route::get('/filter/{id}', 'HomeController@filter')->name("dashboard/filter");
+    /* ----- Login Controller ----- */
+    Route::get('logout', '\App\Http\Controllers\Auth\LoginController@logout');
 
-//For enter function in Auto suggest
-Route::get('/filter', 'HomeController@filterEnter')->name("dashboard/filter");
-
-
-Route::get('/filterName/{id}', 'HomeController@filterName')->name("dashboard/filter");
-
-//AUtocomplete
-
-Route::get('/user/find', 'HomeController@searchUsers');
- 
-Route::get('/editProfile', 'EditProfileController@index')->name('editProfile');
-
-Route::post('/store_editProfile', ['as' => 'store_editProfile', 'uses' => 'EditProfileController@saveStore']);
-
-Route::post('/manu_editrofile', ['as' => 'manu_editProfile', 'uses' => 'EditProfileController@saveManu']);
-
-Route::get('/history', 'HistoryController@index');
-
-Route::get('/opportunities', 'OpportunityController@opportunityWithRanking')->name('opportunities');
-
-Route::get('/slowstock', 'SlowStockController@index');
-
-Route::get('/addlistingFromSlow', 'AddListingsController@addFromSlow');
-
-
-
-//Add new product
-Route::post('/createnewprod', ['as' => 'createnewprod', 'uses' => 'AddNewProductController@savenewprod']);
-
-
-Route::post('create','AddListingsController@insert');
-
-
-
-//File Upload
-Route::get('uploadchoose', function(){
-    return View('uploadchoose'); // Your Blade template name
 });
 
-Route::get('/upload', 'FormuploadController@index');
-
-Route::post('upload', 'FormuploadController@upload');
-
-Route::get('/downloads', 'DownloadFileController@download');
 
 
-// Delete Product
-
-Route::post('removeproduct', 'MyListingsController@remove')->name('mylistings');
-
-Route::post('update', 'MyListingsController@saveprod')->name('mylistings');
-
-Route::get('newprod', function(){
-    return View('newprod'); // Your Blade template name
-});
-
-
-//Autofill Product Type
-Route::get('/autofill/{id}', 'AddListingsController@autofillType')->name("Addlisting/");
-
-
-//Cart Index
-Route::get('/cart', 'CartController@index')->name('cart.index');
-// Add item to cart via get
-Route::get('/addToCart/{id}', 'CartController@addToCart')->name('cart.add-item');
-// Minus quantity function
-Route::post('/updateItem', 'CartController@updateItem')->name('cart.update');
-// Remove Item
-Route::get('/removeItem/{id}', 'CartController@removeItem')->name('cart.remove');
-// Clear Cart
-Route::get('/clearCart', 'CartController@clearCart')->name('cart.clear');
-// Cart Checkout
-Route::post('/checkout', 'CartController@checkout')->name('cart.checkout');
